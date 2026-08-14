643ad6211aaff03ef9092370b02ac8d712c39264
Andrei Hetmanski <drehetm@gmail.com>
2026-08-14 10:54:39 +0300

    Update Neovim Lazy plugins to latest version

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,16 @@
 -- Install and configure `Treesitter` plugin
 
-return  {
+return {
   "nvim-treesitter/nvim-treesitter",
   name = "treesitter",
+  lazy = false,
   build = ":TSUpdate",
-  main = "nvim-treesitter.configs",   -- run require(MAIN).setup(opts)
-  opts = {                -- Configure plugin with user defined options
-    ensure_installed = {  -- List of parsers
+
+  config = function()
+    local treesitter = require("nvim-treesitter")
+
+    -- List of parsers
+    local parsers = {
       "lua",              -- for Neovim config
       "bash",             -- for ZSH files
       "markdown",
@@ -17,13 +21,30 @@ return  {
       "yaml",
       "ini",
       "json",
-    },
-    auto_install = true,  -- Automatically install missing parsers when entering buffer
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-  },
-}
+    }
 
+    treesitter.setup()
+
+    -- Install configured parsers
+    treesitter.install(parsers)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        local filetype = vim.bo[args.buf].filetype
+        local lang = vim.treesitter.language.get_lang(filetype) or filetype
+
+        -- Auto-install missing parser
+        if not vim.treesitter.language.add(lang) then
+          treesitter.install({ lang })
+        end
+
+        -- Enable Treesitter highlighting
+        pcall(vim.treesitter.start, args.buf)
+
+        -- Enable Treesitter indentation
+        vim.bo[args.buf].indentexpr =
+          "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+}
