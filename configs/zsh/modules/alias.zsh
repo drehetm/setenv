829208b19687761e1b5ea0bbd3f617bdc863e83d
Andrei Hetmanski <drehetm@gmail.com>
2026-08-21 10:44:55 +0300

    Fix ZSH aliases

--- configs/zsh/modules/alias.zsh
+++ configs/zsh/modules/alias.zsh
@@ -5,7 +5,12 @@ alias ..='cd ..'
 alias ...='cd ../..'
 
 # Change working directory to 'setenv' repo
-alias cdse="cd ${SETENV_REPO_PATH}"
+alias cdsetenv="cd ${SETENV_REPO_PATH}"
+# Change working directory to `yonc-tools` repo
+alias cdyonctools="cd ~/go/src/gitlab.services.yomobile.in/yonc/backend/yonc-tools"
+
+# Show full `git status`
+alias gsa="gfa && grbom && echo && gst && echo && glo -7"
 
 # Use colorls instead of ls
 alias ls="colorls --git-status --no-group --no-hardlinks --sort-dirs --color=always --dark --time-style='+%y-%m-%d %H:%M:%S'"
